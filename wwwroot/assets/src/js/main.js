d44d0bdb78e0d9fb1a4346ab0b552052f501fabc
Philip Cleary <philip@fanatic.co.uk>
2023-03-03 12:36:40 +0000

    initial commit

---
+++ wwwroot/assets/src/js/main.js
@@ -0,0 +1,19 @@
+
+/**
+ * Example inclusion of an ES6 module
+ */
+// import AlpineJsStore from "./modules/alpinejs/store.js";
+
+// new AlpineJsStore(); // Include our Alpine Store
+/**
+ * END Example inclusion of an ES6 module
+ */
+
+(function () {
+    // Expressions indented
+    console.log("ready!");
+
+    function doSomething() {
+        // Expressions indented
+    }
+})();
---
+++ wwwroot/assets/src/js/modules/alpinejs/store.js
@@ -0,0 +1,8 @@
+// export default class AlpineJs {
+//     constructor() {
+//         // Initialise the store
+//         document.addEventListener("alpine:init", () => {
+//             Alpine.store("menuOpen", false);
+//         });
+//     }
+// }
